898651dd07cf2e24cbfd9e36ac9a8e31c6c4bb41
tosalmanshaikh <tosalmanshaikh@gmail.com>
2021-10-03 11:24:56 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,27 @@
+// const personalDetails = [false, 28, "Urecht, 01-01-1990"];
+
+
+
+const ingredients = ["Geraspte kaas", "Spinazie", "Knoflook", "Olijfolie", "Lasagne bladeren"];
+
+// console.log(ingredients);
+//
+// console.log(ingredients[3]);
+//
+// const step1 = "Doe een beetje " + ingredients[3] + " in de pan.";
+
+// console.log(step1);
+
+ingredients[4] = "Volkoren lasagebladen";
+
+console.log(ingredients);
+
+const ingredientsLength = ingredients.length;
+
+console.log(ingredientsLength);
+
+const lastIndex = ingredients.length - 1;
+console.log(lastIndex);
+console.log(ingredients[lastIndex]);
+
+
