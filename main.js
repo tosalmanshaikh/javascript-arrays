59cf0274d56c110ba5cc3b64f543a17ac4a89104
tosalmanshaikh <tosalmanshaikh@gmail.com>
2021-10-03 15:14:06 +0200

    lot of practice

--- main.js
+++ main.js
@@ -28,21 +28,93 @@
 // console.log(lastIndex);
 // console.log(ingredients[lastIndex]);
 
+//
+// let lasagneIngredients = ["Geraspte kaas", "Spinazie", "Knoflook", "Olijfolie", "Lasage bladen", "Ui"];
+//
+// console.log(lasagneIngredients[5]);
+//
+//
+// lasagneIngredients [0] = "Corona kaas";
+// console.log(lasagneIngredients);
+//
+//
+// const step1 = "Doe een beetje " + lasagneIngredients[0] + " bij";
+// console.log(step1);
+//
+// const ingredientsLength = lasagneIngredients.length;
+// console.log(ingredientsLength -2);
+//
+// lasagneIngredients [3] = "Kokos olie";
+// console.log(lasagneIngredients);
 
-let lasagneIngredients = ["Geraspte kaas", "Spinazie", "Knoflook", "Olijfolie", "Lasage bladen", "Ui"];
 
-console.log(lasagneIngredients[5]);
+const recipe = {
+    name: "spinazie lasage",
+    cookingTime: 45,
+    'is-gluten-free': false,
+    // printInfo: () => {console.log("Dit recept is niet glutenvrij.")}
+    ingredients: ["Spinazie", "Kaas", "Lasagne bladen"],
+    nutritionInfo: {
+        natrium: 34,
+        calories: 670,
+        // printInfo: () => {
+        //     console.log("dit recept is niet glutenvrij.")
+        // }
+    },
+}
+
+console.log(recipe.nutritionInfo);
+
+//
+// if (recipe['isGlutenFree']) {
+//     console.log('Bevat geen gluten!');
+// }
+//
+// for (let i = 0; i < recipe.ingredients.length; i++) {
+//     console.log(recipe.ingredients[i]);
+// }
 
 
-lasagneIngredients [0] = "Corona kaas";
-console.log(lasagneIngredients);
+
+// const key = "name";
+//
+// const person = {
+//     name: "Piet",
+// }
+//
+// console.log(person[key]);
+//
 
 
-const step1 = "Doe een beetje " + lasagneIngredients[0] + " bij";
-console.log(step1);
 
-const ingredientsLength = lasagneIngredients.length;
-console.log(ingredientsLength -2);
 
-lasagneIngredients [3] = "Kokos olie";
-console.log(lasagneIngredients);
+
+
+// console.log(recipe.nutritionInfo);
+
+// recipe.nutritionInfo.calories = 900;
+// recipe.nutritionInfo["calories"] = 1000;
+
+// console.log(recipe.nutritionInfo);
+
+
+// const objectKey = "name";
+// console.log(recipe.objectKey);
+
+
+// console.log(recipe.name);
+
+
+// console.log(recipe["cookingTime"]);
+
+
+// console.log(recipe['is-gluten-free']);
+
+// const amountOfDough = recipe.ingredients[2];
+// console.log(amountOfDough);
+
+//
+// const amountOfCalories = recipe.nutritionInfo.calories + "kCal";
+// console.log(amountOfCalories);
+
+// console.log(recipe.nutritionInfo.calories);
